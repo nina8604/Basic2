13b17712ea561e4f46bcf75f49abbb2dd851fef4
Nina Yaremenko <nina.yaremenko@devit-team.com>
2019-08-27 12:23:08 +0300

    create custom post type

--- wp-content/themes/DevIT-Basis/functions.php
+++ wp-content/themes/DevIT-Basis/functions.php
@@ -114,3 +114,44 @@ function show_form (){
 
 // add shortcode
 add_shortcode ('devit_contact_form', 'show_form');
+
+// register custom post type
+add_action( 'init', 'create_custom_post_type' );
+function create_custom_post_type() {
+    register_post_type ('devit_contact_form', array(
+        'label'  => null,
+        'labels' => array(
+            'name'               => __('Devit contact forms'), // основное название для типа записи
+            'singular_name'      => __('devit_contact_form'), // название для одной записи этого типа
+            'add_new'            => 'Add', // для добавления новой записи
+            'add_new_item'       => 'Add new devit_contact_form', // заголовка у вновь создаваемой записи в админ-панели.
+            'edit_item'          => 'Edit devit_contact_form', // для редактирования типа записи
+            'new_item'           => 'New devit_contact_form', // текст новой записи
+            'view_item'          => 'View devit_contact_form', // для просмотра записи этого типа.
+            'search_items'       => 'Search devit_contact_form', // для поиска по этим типам записи
+            'not_found'          => 'No devit_contact_form found', // если в результате поиска ничего не было найдено
+            'not_found_in_trash' => 'No devit_contact_form found in trash', // если не было найдено в корзине
+        ),
+        'description'         => '',
+        'public'              => true,
+        // 'publicly_queryable'  => null, // зависит от public
+        // 'exclude_from_search' => null, // зависит от public
+        // 'show_ui'             => null, // зависит от public
+        // 'show_in_nav_menus'   => null, // зависит от public
+        'show_in_menu'        => null, // показывать ли в меню адмнки
+        // 'show_in_admin_bar'   => null, // зависит от show_in_menu
+        'show_in_rest'        => null, // добавить в REST API. C WP 4.7
+        'rest_base'           => null, // $post_type. C WP 4.7
+        'menu_position'       => null,
+        'menu_icon'           => null,
+        //'capability_type'   => 'post',
+        //'capabilities'      => 'post', // массив дополнительных прав для этого типа записи
+        //'map_meta_cap'      => null, // Ставим true чтобы включить дефолтный обработчик специальных прав
+        'hierarchical'        => false,
+        'supports'            => [ 'title', 'editor' ], // 'title','editor','author','thumbnail','excerpt','trackbacks','custom-fields','comments','revisions','page-attributes','post-formats'
+        'taxonomies'          => [],
+        'has_archive'         => true,
+        'rewrite'             => true,
+        'query_var'           => true,
+    ) );
+}
