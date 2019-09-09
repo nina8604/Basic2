90107c42af0e18f72b64794b4dd523e0f1e82188
Nina Yaremenko <nina.yaremenko@devit-team.com>
2019-09-09 09:49:48 +0300

    clean page.php

--- wp-content/themes/DevIT-Basis/page.php
+++ wp-content/themes/DevIT-Basis/page.php
@@ -23,28 +23,6 @@ get_header('without-menu');
                 the_post();
 
                 the_content();
-//                ob_start('add_content_title');
-//                $pageHtml = ob_get_contents();
-//                ob_end_clean();
-
-//                echo $pageHtml;
-
-//                $content = apply_filters( 'the_content', 'add_content_title' );
-//                function add_content_title($content) {
-//                    $changes = [
-//                        '<h1 .*>' => '<h1 .*>DevIT - ',
-//                        '<h2 .*>' => '<h2 .*>DevIT - ',
-//                        '<h3 .*>' => '<h3 .*>DevIT - ',
-//                    ];
-//                    foreach($changes as $found => $change){
-//                        $content = str_ireplace($found, $change, $content);
-//                    }
-//                    return $content;
-//                }
-
-
-//                the_content();
-
 
             endwhile; // End of the loop.
             ?>
